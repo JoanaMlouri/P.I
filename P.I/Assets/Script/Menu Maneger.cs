a13a6beeed17d39737cf44e35dc7a75826d3ec4b
JoanaMlouri <jo.mlourinho@gmail.com>
2024-06-06 16:28:40 -0300

    não sei oq foi isso

--- P.I/Assets/Script/Menu Maneger.cs
+++ P.I/Assets/Script/Menu Maneger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MenuManeger : MonoBehaviour
 {
     [SerializeField] private string NomeDoLevelDeJogo;
     [SerializeField] private GameObject painelMeniInicial;
